ed2adabc31e310e4cead46c78096f12ae0a7ae08
Jan Müller <janmueller3698@gmail.com>
2022-08-09 21:25:19 +0200

    fix: start align flex row

--- src/pages/stations/[name].tsx
+++ src/pages/stations/[name].tsx
@@ -165,7 +165,7 @@ const StationPage: NextPage = () => {
             <h1 className='text-3xl sm:text-4xl md:text-5xl'>{stationName}</h1>
             <FavoriteToggle stationName={stationName} isFavorite={isFavorite} />
           </div>
-          <div className='flex flex-1 flex-wrap justify-center m-2'>
+          <div className='flex flex-1 flex-wrap content-start justify-center m-2'>
             {!monitors && <Spinner />}
             {monitors?.length === 0 && (
               <div className='flex-1 flex items-center justify-center'>
