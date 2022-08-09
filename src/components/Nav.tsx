90e6c1023476cda9411b5a441694d6612184b301
Jan Müller <janmueller3698@gmail.com>
2022-08-09 16:49:16 +0200

    refactor: improve nav

--- src/components/Nav.tsx
+++ src/components/Nav.tsx
@@ -5,16 +5,26 @@ import { FC } from 'react'
 
 const Nav: FC = () => {
   const router = useRouter()
+  const targets = [
+    {
+      href: '/',
+      icon: 'fa:home',
+    },
+  ]
   return (
     <nav className='bg-gray-100 border-t-gray-200 border-t-2 fixed left-0 right-0 bottom-0 flex justify-evenly'>
-      <Link href='/' passHref>
-        <a className='p-4'>
-          <Icon
-            icon='fa:home'
-            className={router.pathname !== '/' ? 'text-black' : 'text-gray-500'}
-          />
-        </a>
-      </Link>
+      {targets.map(({ href, icon }) => (
+        <Link href={href} passHref key={href}>
+          <a className='p-4'>
+            <Icon
+              icon={icon}
+              className={
+                router.pathname !== '/' ? 'text-black' : 'text-gray-500'
+              }
+            />
+          </a>
+        </Link>
+      ))}
     </nav>
   )
 }
