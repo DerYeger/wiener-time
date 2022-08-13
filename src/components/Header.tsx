5412c12e9292bb981e6cd2d198336c7d264ff849
Jan Müller <janmueller3698@gmail.com>
2022-08-13 13:01:15 +0200

    feat: link to /about

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,6 +1,7 @@
 import { Icon } from '@iconify/react'
 import { signOut, signIn, useSession } from 'next-auth/react'
 import Image from 'next/future/image'
+import Link from 'next/link'
 import { FC } from 'react'
 
 const Header: FC = () => {
@@ -8,8 +9,8 @@ const Header: FC = () => {
 
   return (
     <header className='py-2 px-4 bg-gray-100 border-b-gray-200 border-b-2 sticky top-0 z-40'>
-      {session.data && (
-        <div className='w-full flex items-center justify-between'>
+      <div className='w-full flex items-center justify-between h-12'>
+        {session.data ? (
           <Image
             src={session.data.user?.image ?? '/favicon.svg'}
             alt={session.data.user?.name ?? 'WienerTime'}
@@ -17,27 +18,34 @@ const Header: FC = () => {
             height={48}
             className='rounded-lg'
           />
-          <button
-            title='Logout'
-            onClick={() => signOut()}
-            className='text-xl text-gray-400 hover:text-gray-600 transition-colors'
-          >
-            <Icon icon='fa:sign-out' />
-          </button>
-        </div>
-      )}
-      {!session.data && (
-        <div className='w-full flex items-center justify-between h-12'>
+        ) : (
           <Image src='/favicon.svg' alt='WienerTime' width={48} height={48} />
-          <button
-            title='Login'
-            onClick={() => signIn()}
-            className='text-xl text-gray-400 hover:text-gray-600 transition-colors'
-          >
-            <Icon icon='fa:sign-in' />
-          </button>
+        )}
+        <div className='flex'>
+          <Link href='/about'>
+            <a className='h-[48px] w-[48px] flex justify-center items-center text-xl text-gray-400 hover:text-gray-600 transition-colors'>
+              <Icon icon='fa:info' />
+            </a>
+          </Link>
+          {session.data ? (
+            <button
+              title='Logout'
+              onClick={() => signOut()}
+              className='h-[48px] w-[48px] flex justify-center items-center text-xl text-gray-400 hover:text-gray-600 transition-colors'
+            >
+              <Icon icon='fa:sign-out' />
+            </button>
+          ) : (
+            <button
+              title='Login'
+              onClick={() => signIn()}
+              className='h-[48px] w-[48px] flex justify-center items-center text-xl text-gray-400 hover:text-gray-600 transition-colors'
+            >
+              <Icon icon='fa:sign-in' />
+            </button>
+          )}
         </div>
-      )}
+      </div>
     </header>
   )
 }
